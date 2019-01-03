b0f862abeff30757161d5852f764e5f78ac81e1b
SebaAgosta <seba.agosta@gmail.com>
2019-01-03 15:53:43 -0300

    Primer servlet

---
+++ src/com/DDSWebApp/jsps/servlets/LoginServlet.java
@@ -0,0 +1,49 @@
+package com.DDSWebApp.jsps.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginWeb.jr")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String usuario = request.getParameter("usuario");
+		String password = request.getParameter("password");
+		if (usuario.equals("Sebastian") && password.equals("Agosta")) {
+			System.out.println("Welcome....");
+			
+		} else  {
+			System.out.println("Error en credenciales de acceso... ");
+			
+		}
+		
+	}
+
+}
